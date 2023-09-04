75aeed3ea94b5b4c397f61bfc56e89a87693c76e
arctic-00 <jake.cornell@protonmail.com>
2023-09-04 07:18:56 -0800

    Implemented more of GPIO controller

--- PC_side/GPIO_Controller.c
+++ PC_side/GPIO_Controller.c
@@ -2,8 +2,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <ctype.h>
+
+#define PIN_NAME_FILE_PATH "./GPIO_Pin_Names.txt"
+
+#define MAX_LINE_LEN 254
 
 ///////////////////////////////// Function Prototypes /////////////////////////////////
 
@@ -16,6 +21,7 @@ int main(int argc, char *argv[]) {
   if (argc != 2 && argc != 3) {
     fprintf(stderr, "Usage:\n");
     fprintf(stderr, "%s <GPIO Pin or Name of item> <Optional: Value (Otherwise value is toggled)>\n", argv[0]);
+    return EXIT_FAILURE;
   }
 
   int pin;
@@ -28,6 +34,8 @@ int main(int argc, char *argv[]) {
 
   } else pin = pin_name_to_number(argv[1]);
 
+  printf("Pin: %d\n", pin);
+
   int fd = port_setup();
 
   if (argc == 2) {
@@ -39,5 +47,16 @@ int main(int argc, char *argv[]) {
 
 
 int pin_name_to_number(char *name) {
+  FILE *fp = fopen(PIN_NAME_FILE_PATH, "r");
   
+  char line[MAX_LINE_LEN + 1];
+  while (fgets(line, MAX_LINE_LEN, fp)) {
+    char *archived_name = strtok(line, ":");
+
+    if (strcmp(archived_name, name) == 0) {
+      return strtol(strtok(NULL, ":"), NULL, 10);
+    }
+  }
+
+  return -1;
 }
